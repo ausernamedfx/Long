b94a2e191129c7cdf2aa6b7dc99bcd59d94f4d7f
ausernamedfx <fx@code.email.cn>
2021-08-31 22:30:36 +0800

    static

--- src/day1/test/StaticTest.java
+++ src/day1/test/StaticTest.java
@@ -1,9 +1,43 @@
 package day1.test;
 
+import javax.swing.text.AsyncBoxView;
+
 public class StaticTest {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+//        System.out.println("hello");
+
+        Chinese c1 = new Chinese();
+        c1.age = 24;
+        c1.name = "LiWei";
+
+        Chinese c2 = new Chinese();
+        c2.age = 27;
+        c2.name = "ZhangWei";
+
+//        c1.nation = "CHN";
+        c2.nation = "China";
+        System.out.println(c2.nation);
+        System.out.println(c1.nation);
+
+        Chinese.show();
+//        Chinese.eat();
 
     }
+}
+
+class Chinese{
+    String name;
+    int age;
+    // 静态变量,静态属性
+    static String nation;
+
+    public void eat(){
+        System.out.println("eat");
+    }
+    public static void show(){
+        System.out.println("hh");
+    }
+
+
 }
