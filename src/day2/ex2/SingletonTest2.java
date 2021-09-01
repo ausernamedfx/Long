2606641df78ee64e51fe4303044598ecf736291a
ausernamedfx <fx@code.email.cn>
2021-09-01 15:12:37 +0800

    懒汉式

---
+++ src/day2/ex2/SingletonTest2.java
@@ -0,0 +1,31 @@
+package day2.ex2;
+
+public class SingletonTest2 {
+    public static void main(String[] args) {
+        Order or1 = Order.getInstance();
+        Order or2 = Order.getInstance();
+        System.out.println(or1 == or2);
+    }
+}
+
+
+//懒汉式
+class Order{
+
+    //私有化构造器
+    private Order(){
+
+    }
+    //声明当前对象.没有初始化
+    private static Order instance = null;
+
+    //
+    public static Order getInstance(){
+        if (instance == null){
+            instance = new Order();
+        }
+
+        return instance;
+    }
+
+}
