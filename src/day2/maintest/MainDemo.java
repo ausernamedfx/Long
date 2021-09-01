ccfd9db592e1e65571c9bde6b9caa2aae78b8d25
ausernamedfx <fx@code.email.cn>
2021-09-01 18:22:14 +0800

    main入口

---
+++ src/day2/maintest/MainDemo.java
@@ -0,0 +1,13 @@
+package day2.maintest;
+
+
+public class MainDemo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("******" + args[i]);
+        }
+
+
+    }
+}
