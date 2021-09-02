262147009eb9328fcc8f8704f02487cf92fc0539
ausernamedfx <fx@code.email.cn>
2021-09-02 22:46:29 +0800

    Abstract

---
+++ src/day3/abstractTest/AbstractTest.java
@@ -0,0 +1,49 @@
+package day3.abstractTest;
+
+// 抽象方法,抽象类
+
+public class AbstractTest {
+    public static void main(String[] args) {
+        // 不可实例化
+//        Person p1 = new Person();
+//        p1.eat();
+
+
+//        Student s1 = new Student();
+//        s1.eat();
+    }
+}
+
+abstract class Person{
+    String name;
+    int age;
+
+    public Person(){
+
+    }
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    // 抽象方法;包含抽象方法的类,一定是一个抽象类
+    public abstract void eat();
+    public void walk(){
+        System.out.println("zou");
+    }
+}
+
+class Creature{
+
+}
+class Student extends Person{
+
+
+    public Student(String name, int age){
+        super(name,age);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
