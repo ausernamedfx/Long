620d941ec1dffe9d44de727c6ad3f50ce3357934
ausernamedfx <fx@code.email.cn>
2021-09-08 13:21:29 +0800

    InnerClass

---
+++ src/day6/InnerClassTest.java
@@ -0,0 +1,58 @@
+package day6;
+
+//内部类
+
+public class InnerClassTest {
+    public static void main(String[] args) {
+
+        Person.Dog test = new Person.Dog();
+
+        test.show();
+
+
+    }
+}
+
+class Person{
+    String name;
+    int age;
+    public void show(){
+        System.out.println("nishuonimane");
+    }
+
+    //静态成员内部类
+    static class Cat{
+
+        private int number;
+
+    }
+
+    //非静态成员内部类
+    static class Dog{
+
+        String name;
+        void show(){
+            System.out.println("gou");
+        }
+    }
+
+    public void method(){
+        class AA{
+
+
+
+        }
+    }
+
+    {
+        class BB{
+
+        }
+
+    }
+    public Person(){
+        class CC{
+
+        }
+    }
+}
